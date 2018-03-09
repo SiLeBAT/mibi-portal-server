1a701d95f22495f7915e6dd0794ba12f15619236
Dominic Tölle <dominic.tolle@gmail.com>
2018-03-09 17:14:00 +0100

    Fix multer upload

--- src/server/sampleManagement/upload/controllers/datasets.ts
+++ src/server/sampleManagement/upload/controllers/datasets.ts
@@ -1,18 +1,18 @@
 import { Response, Request } from 'express';
-import { multerUpload } from './../middleware';
 import { logger } from './../../../../aspects';
 
 function saveDataset(req: Request, res: Response) {
-    multerUpload(req, res, function (err) {
-        if (err) {
-            logger.error('Unable to save Dataset.');
-            return res
-                .status(400)
-                .end();
-        }
-        // FIXME: Temporary redirect to allow for feature parity with old version
-        res.redirect(307, 'validation');
-    });
+    return res.redirect(307, 'validation');
+    // return multerUpload(req, res, function (err) {
+    //     if (err) {
+    //         logger.error('Unable to save Dataset.');
+    //         return res
+    //             .status(400)
+    //             .end();
+    //     }
+    //     // FIXME: Temporary redirect to allow for feature parity with old version
+    //     res.redirect(307, 'validation');
+    // });
 }
 
 export {
