055a960b573018c977c6ca56d4d04b7ed6346fc5
Dominic Tölle <dominic.tolle@gmail.com>
2018-09-04 15:50:06 +0200

    Amend catalog search endpoint, to allow uploading of catalog
    Ticket: #52

--- src/app/sampleManagement/application/catalog-search.service.ts
+++ src/app/sampleManagement/application/catalog-search.service.ts
@@ -1,14 +1,14 @@
 
 import * as _ from 'lodash';
-import { ICatalogPort, ISearchAlias } from './catalog.service';
+import * as Fuse from 'fuse.js';
 
-export interface ISearchOptions extends Fuse.FuseOptions {
-    useEnhanced: boolean;
-    options: Fuse.FuseOptions;
+interface ISearchResult {
+    item: string;
+    score: number;
 }
-
 export interface ICatalogSearchPort {
-    searchCatalog(catalogName: string, searchTerm: string, searchOptions: Fuse.FuseOptions, useEnhanced: boolean): void;
+    // tslint:disable-next-line:no-any
+    searchCatalog(catalog: any[], searchTerms: string[], searchOptions: Fuse.FuseOptions): void;
 }
 
 export interface ICatalogSearchService extends ICatalogSearchPort {
@@ -16,32 +16,18 @@ export interface ICatalogSearchService extends ICatalogSearchPort {
 
 class CatalogSearchService implements ICatalogSearchService {
 
-    constructor(private catalogService: ICatalogPort) { }
-    searchCatalog(catalogName: string, searchTerm: string, searchOptions: Fuse.FuseOptions, useEnhanced: boolean) {
-
-        // tslint:disable-next-line:no-any
-        let enhancements: any[] = [];
-        if (useEnhanced) {
-            // FIXME: Map callback is hard coded & dependent on catalog: Need to refactor Catalog data structure: generalize fields
-            if (catalogName !== 'adv16') {
-                throw new Error(`Enhancements for Catalog ${catalogName} not supported`);
-            }
-            enhancements = _(this.catalogService.getCatalogSearchAliases(catalogName))
-                .map((e: ISearchAlias) => {
-                    return e.alias.map(alias => ({
-                        Text1: e.token,
-                        'P-Code3': alias
-                    }));
-                })
-                .flattenDeep()
-                .value();
-        }
-        const fuse = this.catalogService.getCatalog(catalogName).getFuzzyIndex(searchOptions, enhancements);
-        return fuse.search(searchTerm);
+    // tslint:disable-next-line:no-any
+    searchCatalog(catalog: any[], searchTerms: string[], searchOptions: Fuse.FuseOptions) {
+        const fuse = new Fuse(catalog, searchOptions);
+        const result = searchTerms.reduce((acc: Record<string, ISearchResult[]>, t) => {
+            acc[t] = fuse.search(t);
+            return acc;
+        }, {});
+        return result;
     }
 
 }
 
-export function createService(service: ICatalogPort): ICatalogSearchService {
-    return new CatalogSearchService(service);
+export function createService(): ICatalogSearchService {
+    return new CatalogSearchService();
 }
