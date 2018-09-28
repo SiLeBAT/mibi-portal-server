bd431e2bb12ef2b9ec59c5d152fb6ca3ee3560f3
Dominic Tölle <dominic.tolle@gmail.com>
2018-09-28 13:58:11 +0200

    Remove catalog-search optimization endpoint
    Ticket: #57

--- src/app/sampleManagement/application/index.ts
+++ src/app/sampleManagement/application/index.ts
@@ -1,5 +1,4 @@
 export { IFormValidatorPort, IFormValidatorService, createService as createFormValidationService } from './formValidation.service';
 export { IFormAutoCorrectionPort, IFormAutoCorrectionService, createService as createFormAutoCorrectionService } from './formAutoCorrection.service';
 export { ICatalogService, ICatalogPort, createService as createCatalogService } from './catalog.service';
-export { ICatalogSearchService, ICatalogSearchPort, createService as createCatalogSearchService } from './catalog-search.service';
 export { IDatasetPort, IDatasetService, createService as createDatasetService } from './dataset.service';
--- src/ui/server/routes/util/index.ts
+++ src/ui/server/routes/util/index.ts
@@ -5,8 +5,6 @@ import { getRouter as getBaseRouter, RouterType } from '../routerProvider';
 
 function getRouter(controllerFactory: IControllerFactory) {
     const router = express.Router();
-    logger.verbose('Registering Route', { route: '/catalog-search' });
-    router.use('/catalog-search', getBaseRouter(RouterType.CATALOG_SEARCH, controllerFactory));
     logger.verbose('Registering Route', { route: '/system-info' });
     router.use('/system-info', getBaseRouter(RouterType.SYSTEM_INFO, controllerFactory));
     return router;
