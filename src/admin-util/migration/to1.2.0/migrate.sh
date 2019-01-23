fc02137288e5875ad4bcc295e5f5496863750165
Dominic Tölle <dominic.tolle@gmail.com>
2019-01-23 14:40:49 +0100

    Testing db import pipeline 2

--- src/admin-util/migration/to1.2.0/migrate.sh
+++ src/admin-util/migration/to1.2.0/migrate.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# To be used with master-data v1.0.0
+# To be used with master-data v1.2.0
 # Make sure the $CONFIG_DIR points towards the directory containing the environment specific config file
 # Make sure the $DATA_DIR points towards the directory containing the appropriate data (see master-data version above)
 
 CONFIG_DIR=../../../../config
-DATA_DIR=../../../../db
 SCRIPT_DIR=../../scripts
 
 # Importing new Master Data
-NODE_CONFIG_DIR=$CONFIG_DIR node $SCRIPT_DIR/importCollection.js $DATA_DIR/validation-errors/validationerrors.json
-NODE_CONFIG_DIR=$CONFIG_DIR node $SCRIPT_DIR/importCollection.js $DATA_DIR/nrls/nrls.json
+NODE_CONFIG_DIR=$CONFIG_DIR node $SCRIPT_DIR/importCollection.js ../validation-errors/validationerrors.json
+NODE_CONFIG_DIR=$CONFIG_DIR node $SCRIPT_DIR/importCollection.js ../nrls/nrls.json
