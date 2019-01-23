8c61f261a550554f473cd8f1e4ea8f82abf4d174
Dominic Tölle <dominic.tolle@gmail.com>
2019-01-23 15:32:09 +0100

    Testing db import pipeline 6

--- src/admin-util/migration/to1.2.0/migrate.sh
+++ src/admin-util/migration/to1.2.0/migrate.sh
@@ -5,7 +5,7 @@
 # Make sure the $DATA_DIR points towards the directory containing the appropriate data (see master-data version above)
 CONFIG_DIR=../../../../config
 SCRIPT_DIR=../scripts
-DATA_DIR= ./db
+DATA_DIR=./db
 
 # Importing new Master Data
 NODE_CONFIG_DIR=$CONFIG_DIR node $SCRIPT_DIR/importCollection.js $DATA_DIR/validation-errors/validationerrors.json
