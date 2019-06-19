13e4dddb223a09af31c04a1726bdf8313510918b
Dominic Tölle <dominic.tolle@gmail.com>
2019-06-19 11:04:31 +0200

    Fixed validation with annotated data bug
    Ticket: #118
    Change-Id: I8b07f3c6d2e1c9725e01b58c11a928d3304e4dca

--- src/ui/server/model/shared-dto.model.ts
+++ src/ui/server/model/shared-dto.model.ts
@@ -1,58 +1,58 @@
 interface AddressDTO {
-    readonly instituteName: string;
-    readonly department?: string;
-    readonly street: string;
-    readonly zip: string;
-    readonly city: string;
-    readonly contactPerson: string;
-    readonly telephone: string;
-    readonly email: string;
+    instituteName: string;
+    department?: string;
+    street: string;
+    zip: string;
+    city: string;
+    contactPerson: string;
+    telephone: string;
+    email: string;
 }
 
 interface AnalysisDTO {
-    readonly species: boolean;
-    readonly serological: boolean;
-    readonly phageTyping: boolean;
-    readonly resistance: boolean;
-    readonly vaccination: boolean;
-    readonly molecularTyping: boolean;
-    readonly toxin: boolean;
-    readonly zoonosenIsolate: boolean;
-    readonly esblAmpCCarbapenemasen: boolean;
-    readonly other: string;
-    readonly compareHuman: boolean;
+    species: boolean;
+    serological: boolean;
+    phageTyping: boolean;
+    resistance: boolean;
+    vaccination: boolean;
+    molecularTyping: boolean;
+    toxin: boolean;
+    zoonosenIsolate: boolean;
+    esblAmpCCarbapenemasen: boolean;
+    other: string;
+    compareHuman: boolean;
 }
 export interface SampleSetMetaDTO {
-    readonly nrl: string;
-    readonly sender: AddressDTO;
-    readonly analysis: AnalysisDTO;
-    readonly urgency: string;
-    readonly fileName?: string;
+    nrl: string;
+    sender: AddressDTO;
+    analysis: AnalysisDTO;
+    urgency: string;
+    fileName?: string;
 }
 
 interface SampleValidationErrorDTO {
-    readonly code: number;
-    readonly level: number;
-    readonly message: string;
+    code: number;
+    level: number;
+    message: string;
 }
 
 export interface SampleDataEntryDTO {
-    readonly value: string;
-    readonly errors?: SampleValidationErrorDTO[];
-    readonly correctionOffer?: string[];
-    readonly oldValue?: string;
+    value: string;
+    errors?: SampleValidationErrorDTO[];
+    correctionOffer?: string[];
+    oldValue?: string;
 }
 export interface SampleDataDTO {
-    readonly [key: string]: SampleDataEntryDTO;
+    [key: string]: SampleDataEntryDTO;
 }
 
 export interface SampleDataContainerDTO {
-    readonly sample: SampleDataDTO;
+    sample: SampleDataDTO;
 }
 
 export interface SampleSetDTO {
-    readonly samples: SampleDataContainerDTO[];
-    readonly meta: SampleSetMetaDTO;
+    samples: SampleDataContainerDTO[];
+    meta: SampleSetMetaDTO;
 }
 
 export interface OrderDTO {
