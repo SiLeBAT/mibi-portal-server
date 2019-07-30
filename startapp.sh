f608ec871364990bbe2ced62e592bd7f8120f444
Dominic Tölle <dominic.tolle@gmail.com>
2019-07-30 11:34:06 +0200

    Change log file name
    Ticket: #137
    Change-Id: I391a80857bba9471e758d2c9fe8c30761ac14398

--- startapp.sh
+++ startapp.sh
@@ -5,6 +5,10 @@ if [ "$#" -ne 1 ]; then
   LOG_FILE=./mibi_output
 fi
 
-forever -l $LOG_FILE.log -a start lib/main.js
+BASE_NAME=`basename $LOG_FILE .log`
+DIR_NAME=`dirname $LOG_FILE`
+ADMIN_LOG=$DIR_NAME/$BASE_NAME-admin.log
+
+forever -l $LOG_FILE -a start lib/main.js
 cp ./config/config.js ./node_modules/mongo-express/
-ME_CONFIG_MONGODB_URL='mongodb://localhost:27017/epilab' forever -l $LOG_FILE-admin.log -a start ./node_modules/mongo-express/app.js
+ME_CONFIG_MONGODB_URL='mongodb://localhost:27017/epilab' forever -l $ADMIN_LOG -a start ./node_modules/mongo-express/app.js
