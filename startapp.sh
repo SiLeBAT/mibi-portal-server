53bbbe8d9eef9805929c0d6ebab6bc2f4dd9443e
Dominic Tölle <dominic.tolle@gmail.com>
2019-07-30 11:34:06 +0200

    Change log file name
    Change-Id: I8438d6f680f14b0083ac6d1dc59293d58e07ff2a

--- startapp.sh
+++ startapp.sh
@@ -5,6 +5,6 @@ if [ "$#" -ne 1 ]; then
   LOG_FILE=./mibi_output
 fi
 
-forever -l $LOG_FILE.log -a start lib/main.js
+forever -l $LOG_FILE -a start lib/main.js
 cp ./config/config.js ./node_modules/mongo-express/
-ME_CONFIG_MONGODB_URL='mongodb://localhost:27017/epilab' forever -l $LOG_FILE-admin.log -a start ./node_modules/mongo-express/app.js
+ME_CONFIG_MONGODB_URL='mongodb://localhost:27017/epilab' forever -l admin-$LOG_FILE -a start ./node_modules/mongo-express/app.js
