3ea71c1bba4d901a2bca0898c1acd73a9e6013ad
Isaak Gerber <isaak.gerber@bfr.bund.de>
2019-09-09 13:22:48 +0200

    Fixed summertime issue caused by JSON to Excel conversion
    Changed date conversion again
    Added tests for samples endpoint
    
    Ticket: #155

---
+++ src/ui/server/controllers/__mocks__/multer.mock.ts
@@ -0,0 +1,11 @@
+export function addMulterSingleFileFormToMockRequest(req: {}, fieldName: string, file: Buffer, fileName: string): {file: {}} {
+    const multerMock = {
+        fieldname: fieldName,
+        originalname: fileName,
+        encoding: '7bit',
+        mimetype: 'application/octet-stream',
+        size: file.buffer.byteLength,
+        buffer: file
+    };
+    return {...req, file: multerMock};
+};
