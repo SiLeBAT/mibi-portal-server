37d78ecb0888ac8d10ffad6a48c223b0d7755784
Birgit Lewicki <lewicki.birgit@gmail.com>
2020-02-13 14:44:13 +0100

    Adapt start and stop scripts
    Change-Id: I668ea99aeefdaa2c2f141fb03ab9ce9389504ed6

--- startapp.sh
+++ startapp.sh
@@ -5,4 +5,7 @@ if [ "$#" -ne 1 ]; then
   LOG_FILE=./fcl_output
 fi
 
+BASE_NAME=`basename $LOG_FILE .log`
+DIR_NAME=`dirname $LOG_FILE`
+
 forever -l $LOG_FILE.log -a start lib/main.js
--- stopapp.sh
+++ stopapp.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 forever stop ./lib/main.js
-forever stop ./node_modules/mongo-express/app.js 
