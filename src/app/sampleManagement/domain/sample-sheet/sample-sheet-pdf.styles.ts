85f6e5df355a46397fcd3d7ad1e80cb9811e2898
Isaak Gerber <isaak.gerber@bfr.bund.de>
2021-09-03 15:43:14 +0200

    Use pdfmake superscript feature
    Ticket: #245
    Change-Id: I6042bfc55175a5b4bd1a4304791749a209c33f0a

--- src/app/sampleManagement/domain/sample-sheet/sample-sheet-pdf.styles.ts
+++ src/app/sampleManagement/domain/sample-sheet/sample-sheet-pdf.styles.ts
@@ -1,12 +1,1 @@
-export const sampleSheetPDFStyles = {
-    defaultSupScript: {
-        fontSize: 6
-    },
-    titleSupScript: {
-        fontSize: 10
-    },
-    heading1SupScript: {
-        fontSize: 8,
-        decoration: ''
-    }
-};
+export const sampleSheetPDFStyles = {};
