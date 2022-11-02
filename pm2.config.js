f050325eafcc843af144d090658a43c983b44759
birgitlewicki <lewicki.birgit@gmail.com>
2022-11-02 17:10:05 +0100

    Parse: Make dashboard available.
    Ticket: #292

--- pm2.config.js
+++ pm2.config.js
@@ -9,5 +9,32 @@ module.exports = {
         env: {
             NODE_APP_INSTANCE: ""
         }
+    }, {
+        name: "mibi-parse-server",
+        script: "./node_modules/.bin/parse-server",
+        args: "./config/parse-server-config.json",
+        exp_backoff_restart_delay: 500,
+        out_file: process.env['MIBI_LOG'],
+        error_file: process.env['MIBI_LOG'],
+        log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+        env: {
+            PARSE_SERVER_MOUNT_PATH: '/admin/parse'
+        }
+    }, {
+        name: "mibi-parse-dashboard",
+        script: "./node_modules/.bin/parse-dashboard",
+        args: "--config ./config/parse-dashboard-config.json --host '127.0.0.1' --mountPath '/admin'",
+        exp_backoff_restart_delay: 500,
+        out_file: process.env['MIBI_LOG'],
+        error_file: process.env['MIBI_LOG'],
+        log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+    }, {
+        name: 'mibi-parse-proxy',
+        script: "./lib/ui/server/parse.proxy.server.js",
+        exp_backoff_restart_delay: 500,
+        out_file: process.env['MIBI_LOG'],
+        error_file: process.env['MIBI_LOG'],
+        log_date_format: "YYYY-MM-DD HH:mm:ss Z",
     }]
 }
+
