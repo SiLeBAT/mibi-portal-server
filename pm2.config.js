ba64b4bc0cc98402eb274d80c7b292ad43caff2d
birgitlewicki <lewicki.birgit@gmail.com>
2023-04-21 13:02:01 +0200

    Fix libs

--- pm2.config.js
+++ pm2.config.js
@@ -25,9 +25,7 @@ module.exports = {
         out_file: process.env['MIBI_LOG'],
         error_file: process.env['MIBI_LOG'],
         log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-    },
-
-    {
+    }, {
         name: "mibi-portal",
         script: "./lib/main.js",
         exp_backoff_restart_delay: 500,
