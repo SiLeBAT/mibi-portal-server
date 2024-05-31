2de86e642654e4cf9c38f37591c9c4e461742c0e
Dominic Tölle <dominic.tolle@gmail.com>
2024-05-31 11:02:50 +0200

    Redirect login to mibi-portal.bfr.bund.de
    Ticket: #414

--- src/ui/server/controllers/institutes.controller.ts
+++ src/ui/server/controllers/institutes.controller.ts
@@ -21,18 +21,13 @@ enum INSTITUTES_ROUTE {
 export class DefaultInstituteController
     extends RedirectionController
     implements InstitutesController {
-
-    constructor(
-    ) {
-        super();
-    }
     @httpGet('/')
     async getInstitutes(@request() req: Request, @response() res: Response) {
         logger.info(
             `${this.constructor.name}.${this.getInstitutes.name}, Request received`
         );
 
-        const institutes = await this.redirectionTarget.get<InstituteCollectionDTO, AxiosResponse<InstituteCollectionDTO>, InstituteCollectionDTO>(API_ROUTE.V2 + INSTITUTES_ROUTE.ROOT).then((response) => {
+        const institutes = await this.redirectionTarget.get<InstituteCollectionDTO, AxiosResponse<InstituteCollectionDTO>, InstituteCollectionDTO>(INSTITUTES_ROUTE.ROOT).then((response) => {
             return response.data;
         })
             .catch(error => {
