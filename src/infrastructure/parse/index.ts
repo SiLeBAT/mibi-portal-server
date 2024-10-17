fbd5e18288bddd218d33b89c4f7dc01283d9d542
Dominic Tölle <dominic.tolle@gmail.com>
2024-10-17 09:53:55 +0200

    WIP

--- src/infrastructure/parse/index.ts
+++ src/infrastructure/parse/index.ts
@@ -1,9 +1,9 @@
-const FSFilesAdapter = require('@parse/fs-files-adapter');
 import config from 'config';
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { ParseServer } from 'parse-server';
 import { logger } from './../../aspects';
-
+const FSFilesAdapter = require('@parse/fs-files-adapter');
 // tslint:disable-next-line: no-any
 const parseServer: any = config.get('parseServer');
 const dataDir: string = config.get('dataStore.dataDir');
@@ -11,7 +11,38 @@ const fsAdapter = new FSFilesAdapter({
     filesSubDirectory: '../' + dataDir
 });
 const app = express();
-const parseConfig = { ...parseServer, filesAdapter: fsAdapter };
+
+const transporter = nodemailer.createTransport({
+    sendmail: true,
+    newline: 'unix',
+    path: '/usr/sbin/sendmail'
+});
+
+const parseConfig = {
+    ...parseServer,
+    filesAdapter: fsAdapter,
+    emailAdapter: {
+        module: 'parse-server-api-mail-adapter',
+        options: {
+            // The email address from which emails are sent.
+            sender: 'sender@example.com',
+            templates: {
+                exampleTemplate: {
+                    subjectPath: './lib/files/custom_email_subject.txt',
+                    textPath: './lib/files/custom_email.txt',
+                    htmlPath: './lib/files/custom_email.html'
+                }
+            },
+            // tslint:disable-next-line: no-any
+            apiCallback: async ({ payload }: { payload: any }) => {
+                const customPayload = {
+                    ...payload
+                };
+                await transporter.sendMail(customPayload);
+            }
+        }
+    }
+};
 const server = new ParseServer(parseConfig);
 
 // Start server
